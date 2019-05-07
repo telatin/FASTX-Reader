cc6e041c1d1bc14ac89aceb7f614bbdd5379ec2d
andrea@telatin.com <andrea@telatin.com>
2019-05-07 13:28:01 +0100

    Allowing user inputs

--- scripts/MooseReader.pl
+++ scripts/MooseReader.pl
@@ -7,8 +7,10 @@ use FindBin qw($Bin);
 use lib "$Bin/../lib/";
 use FASTQ::MooseReader;
 
-my $file1 = "$Bin/test.fastq";
-my $file2 = "$Bin/test2.fastq";
+
+my ($opt_1, $opt_2) = @ARGV;
+my $file1 = $opt_1 // "$Bin/test.fastq";
+my $file2 = $opt_2 // "$Bin/test2.fastq";
 my $o1 = FASTQ::Reader->new(filename => "$file1");
 my $o2 = FASTQ::Reader->new(filename => "$file2");
 
--- scripts/Reader.pl
+++ scripts/Reader.pl
@@ -7,8 +7,9 @@ use FindBin qw($Bin);
 use lib "$Bin/../lib/";
 use FASTQ::Reader;
 
-my $file1 = "$Bin/test.fastq";
-my $file2 = "$Bin/test2.fastq";
+my ($opt_1, $opt_2) = @ARGV;
+my $file1 = $opt_1 // "$Bin/test.fastq";
+my $file2 = $opt_2 // "$Bin/test2.fastq";
 my $o1 = FASTQ::Reader->new({filename => "$file1"});
 my $o2 = FASTQ::Reader->new({filename => "$file2"});
 
