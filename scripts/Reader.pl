fe18e33762e580ba2148c20ea91a458d4994f741
andrea@telatin.com <andrea@telatin.com>
2019-05-07 10:27:04 +0100

    Reader.pm fixed for FASTQ

--- scripts/Reader.pl
+++ scripts/Reader.pl
@@ -7,9 +7,21 @@ use FindBin qw($Bin);
 use lib "$Bin/../lib/";
 use FASTQ::Reader;
 
-my $o = FASTQ::Reader->new(filename => "$Bin/test.fastq");
-print while readline $o->fh;
+my $file1 = "$Bin/test.fastq";
+my $file2 = "$Bin/test2.fastq";
+my $o1 = FASTQ::Reader->new(filename => "$file1");
+my $o2 = FASTQ::Reader->new(filename => "$file2");
 
+
+print "READ FILE 1: $file1\n";
+print "READ FILE 2: $file1\n";
+
+my $counter = 0;
+while (my $seq = $o1->getRead()) {
+  $counter++;
+  my $pair = $o2->getRead();
+  say color('red'), $counter, color('reset'), "\t",$seq->{name}, ' - ', $pair->{name};
+}
 # Test general settings for the module
 #my $file = FASTQ::Reader->new(
 #	filepath => $input,
