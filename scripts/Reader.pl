098c2e4e3a1a73ac7bfe4f07462d57d3fc27a054
Andrea Telatin <andrea@telatin.com>
2019-05-15 16:56:13 +0200

    Testing TravisCI

--- scripts/Reader.pl
+++ scripts/Reader.pl
@@ -1,3 +1,4 @@
+#!/usr/bin/env perl
 use 5.012;
 use autodie;
 use Term::ANSIColor;
@@ -5,39 +6,83 @@ use Data::Dumper;
 use Carp qw(confess);
 use FindBin qw($Bin);
 use lib "$Bin/../lib/";
-use FASTQ::Reader;
+use FASTX::Reader;
+say STDERR color('bold'), "TEST FASTA/FASTQ READER", color('reset');
+say STDERR color('bold'), "Read FASTA/FASTQ files, printing them back to the user", color('reset');
+say STDERR "Usage: $0 FILE1 FILE2 ... FILEn\n";
 
-my ($opt_1, $opt_2) = @ARGV;
-my $file1 = $opt_1 // "$Bin/test.fastq";
-my $file2 = $opt_2 // "$Bin/test2.fastq";
-my $o1 = FASTQ::Reader->new({filename => "$file1"});
-my $o2 = FASTQ::Reader->new({filename => "$file2"});
+# Read two samples files if the user didnt provide any filename
+push(@ARGV,"$Bin/test.fastq", "$Bin/test.fasta" )
+  unless ($ARGV[0]);
 
-
-print STDERR color('yellow'), "Test file formats of input files\n", color('reset');
-foreach my $file (@ARGV) {
-  say " - [",  FASTQ::Reader::getFileFormat($file) , "]\t<- $file";
-}
-print STDERR color('yellow'), "READ FILE 1: $file1\n", color('reset');
-print STDERR color('yellow'), "READ FILE 2: $file1\n", color('reset');
-
-my $counter = 0;
-while (my $seq = $o1->getRead()) {
-  $counter++;
-  if (defined $seq->{qual}) {
-    print '@', $seq->{name}, ' ', $seq->{comment}, "\n", $seq->{seq}, "\n+\n", $seq->{qual}, "\n";
+foreach my $input_file (@ARGV) {
+  # Skip non existing files
+  if ( not -e "$input_file" and "$input_file" ne 'STDIN') {
+    say STDERR color('red'), "] Skipping: $input_file (not exists)", color('reset');
+    next;
   } else {
-    print ">", $seq->{name}, ' ', $seq->{comment}, "\n", $seq->{seq}, "\n";
+    my $seq_reader;
+    if ($input_file eq 'STDIN') {
+      say STDERR color('bold'), 'Reading STDIN', color('reset');
+      $seq_reader = FASTX::Reader->new();
+    } else {
+      $seq_reader = FASTX::Reader->new({filename => "$input_file"});
+    }
+    # Parse the existing files (no check on file type?)
+    say STDERR color('yellow'), "] Reading: $input_file", color('reset');
+
+    my $counter = 0;
+    while (my $seq = $seq_reader->getRead()) {
+      $counter++;
+      # Print FASTA or FASTQ accordingly (check 'qual' defined)
+      if (defined $seq->{qual}) {
+        print '@', $seq->{name}, ' ', $seq->{comment}, "\n", $seq->{seq}, "\n+\n", $seq->{qual}, "\n";
+      } else {
+        print ">", $seq->{name}, ' ', $seq->{comment}, "\n", $seq->{seq}, "\n";
+      }
+    }
+    say STDERR color('green'), "] Finished $input_file: $counter sequences", color('reset') if ($counter);
   }
 }
-say STDERR 'File1 seqs: ',color('green'), $o1->{counter}, color('reset');
-say STDERR 'File2 seqs: ',color('green'), $o2->{counter}, color('reset');
-# Test general settings for the module
-#my $file = FASTQ::Reader->new(
-#	filepath => $input,
-#);
-#my $input = file("$Bin/test.fastq");
 
-#while (my $line = $file->process_file) {
-#	say $line;
-#}
+=head1 NAME
+
+B<reader.pl> - A minimal implementation of the FASTX::Reader module to show how to parse a FASTA/FASTQ files
+
+=head1 USAGE
+
+  reader.pl FILE1 FILE2 ... FILE{n}
+
+If no arguments are supplied, it will parse two test files contained in the script directory
+
+=head1 NOTES
+
+The printed sequences can be slightly different from the input file as the header will be C<{name}{space}{comments}>, but any white space (including a tab) could be the
+comment separator
+
+=head1 WEBSITES
+
+=over 4
+
+=item L<https://github.com/telatin/FASTQ-Parser>
+
+
+The B<GitHub> repository for this module
+
+=item L<https://metacpan.org/pod/FASTX::Reader>
+
+The B<MetaCPAN> page for this module
+
+=back
+
+=head1 AUTHORS
+
+=over 4
+
+=item Andrea Telatin
+
+=item Fabrizio Levorin
+
+=back
+
+=cut
