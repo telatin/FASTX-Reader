69851d5aeaf5e7af8d55513ed4499a8909ee4f15
Andrea Telatin <15690844+telatin@users.noreply.github.com>
2023-01-12 15:51:47 +0000

    Pre-1.9.0

--- lib/FASTX/Seq.pm
+++ lib/FASTX/Seq.pm
@@ -9,7 +9,7 @@ $Data::Dumper::Sortkeys = 1;
 use File::Basename;
 
 $FASTX::Seq::VERSION = $FASTX::Reader::VERSION;
-
+$FASTX::Seq::DEFAULT_QUALITY = 'I';
 require Exporter;
 our @ISA = qw(Exporter);
 
@@ -163,7 +163,8 @@ sub len {
 
 =head2 rc()
 
-Reverse and complement the sequence.
+Reverse and complement the sequence B<in place> (as it's revesible).
+Supports IUPAC degenerate bases.
 
     my $rc = $fastq->rc();
     
@@ -182,10 +183,58 @@ sub rc {
     return $self;
 }
 
+=head2 slice()
 
-=head2 asfasta()
+Retrieve a slice of the sequence (from, length), as perl's substr.
+The change is not in place, will return a new object.
 
-Return the sequence as a FASTA string.
+    my $slice = $fastq->slice(0, 200);
+    
+=cut
+
+sub slice {
+    # Update comment
+    my ($self, $from, $len) = @_;
+    my $new_seq;
+    my $new_qual;
+    if (defined($len)) {
+      $new_seq = substr($self->{seq}, $from, $len);
+      $new_qual = defined($self->{qual}) ? substr($self->{qual}, $from, $len) : undef;
+    } else {
+        $new_seq = substr($self->{seq}, $from);
+        $new_qual = defined($self->{qual}) ? substr($self->{qual}, $from) : undef;
+    }
+    return __PACKAGE__->new($new_seq, $self->{name}, $self->{comment}, $new_qual);
+}
+
+
+=head2 string()
+
+Return the sequence as a string. If arguments are provided, they
+will be treated as FASTA or FASTQ specific according to the record format.
+
+    print $seq->string();
+    
+=cut
+
+sub string {
+    # Update comment
+    my ($self, @args) = @_;
+    my $space = length($self->{comment}) > 0 ? " " : "";
+    my $name  = $self->{name} // "sequence";
+    my $comment = length($self->{comment}) > 0 ? " " . $self->{comment} : "";
+    if (defined $self->{qual}) {
+        return $self->asfastq(@args);
+    } else {
+        return $self->asfasta(@args);
+    }
+    
+}
+
+=head2 asfasta([length]])
+
+Return the sequence as a FASTA string. If the I<length> is provided, the
+sequence will be split into lines of that length.
 
     my $fasta = $seq->asfasta();
     
@@ -193,17 +242,19 @@ Return the sequence as a FASTA string.
 
 sub asfasta {
     # Update comment
-    my ($self) = @_;
+    my ($self, $len) = @_;
     my $space = length($self->{comment}) > 0 ? " " : "";
     my $name  = $self->{name} // "sequence";
     my $comment = length($self->{comment}) > 0 ? " " . $self->{comment} : "";
-    return ">" . $name . $comment . "\n" . $self->{seq} . "\n";
+    return ">" . $name . $comment . "\n" . _split_string($self->{seq}, $len) . "\n";
 }
 
 =head2 asfastq()
 
 Return the sequence as a FASTQ string. Will use a dummy fixed value quality
-if the sequence didnt have a quality string.
+if the sequence didnt have a quality string. 
+Provide an character to use it as quality value, B<will override> the record quality,
+if it has one.
 
     my $fasta = $seq->asfastq();
     
@@ -211,11 +262,113 @@ if the sequence didnt have a quality string.
 
 sub asfastq {
     # Update comment
-    my ($self) = @_;
+    my ($self, $user_quality) = @_;
+    my $quality = $self->{qual};
+
+    if (defined $user_quality) {
+        # User requests new quality
+        if (length($user_quality) == 1 ) {
+            # And it's valid!
+            $quality = $user_quality x length($self->{seq});
+        } else {
+            say STDERR "[WARNING] FASTX::Seq->as_fastq(): Provide a _char_ as quality, not a value (", $user_quality,"): defaulting to $FASTX::Seq::DEFAULT_QUALITY";
+            $quality = $FASTX::Seq::DEFAULT_QUALITY x length($self->{seq});
+        }
+    } elsif (not defined $quality) {
+        $quality = $FASTX::Seq::DEFAULT_QUALITY x length($self->{seq});
+    }
+    
+    
     my $name  = $self->{name} // "sequence";
     my $comment = length($self->{comment}) > 0 ? " " . $self->{comment} : "";
-    my $quality = defined $self->{qual} ? " " . $self->{qual} : "I" x length($self->{seq});
-    return "@" . $name . $comment . "\n" . $self->{seq} . "\n+\n" . $quality;
+    
+    return "@" . $name . $comment . "\n" . $self->{seq} . "\n+\n" . $quality  . "\n";
+}
+
+
+=head2 as_string()
+
+Alias to string()
+    
+=cut
+
+sub as_string {
+    # Update comment
+    my ($self) = @_;
+    return $self->string();
+}
+
+
+=head2 as_fasta()
+
+Alias to asfasta()
+    
+=cut
+
+sub as_fasta {
+    # Update comment
+    my ($self, @args) = @_;
+    return $self->asfasta(@args);
+}
+
+=head2 as_fastq()
+
+Alias to asfastq()
+    
+=cut
+
+sub as_fastq {
+    # Update comment
+    my ($self, @args) = @_;
+    return $self->asfastq(@args);
+}
+
+=head2 is_fasta()
+
+Return true if the record has not a quality value stored (FASTA)
+
+    if ( $seq->is_fasta() ) {
+        ...
+    }
+    
+=cut
+
+sub is_fasta {
+    # Return true if record has no quality
+    my ($self) = @_;
+    return ((defined $self->{qual}) > 0 and length($self->{qual}) == length($self->{seq})) ? 0 : 1;
+}
+
+=head2 is_fastq()
+
+Return true if the record has a quality value stored (FASTQ)
+
+    if ( $seq->is_fastq() ) {
+        ...
+    }
+    
+=cut
+
+sub is_fastq {
+    # Update comment
+    my ($self) = @_;
+    return ((defined $self->{qual}) > 0 and length($self->{qual}) == length($self->{seq})) ? 1 : 0;
+}
+
+
+sub _split_string {
+	my ($string, $width) = @_;
+	if (not defined $width) {
+		return "$string";
+	}
+
+	my $output_string;
+	for (my $i=0; $i<length($string); $i+=$width) {
+		my $frag = substr($string, $i, $width);
+		$output_string.=$frag."\n";
+	}
+    chomp($output_string);
+	return $output_string;
 }
 
 1;
